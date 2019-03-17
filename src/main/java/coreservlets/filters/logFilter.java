337c85092ede64e441f295af2e7e15a41b26c5b6
kyao233 <kyao233@163.com>
2019-03-17 18:25:29 +0800

    add servlet filter

---
+++ src/main/java/coreservlets/filters/logFilter.java
@@ -0,0 +1,45 @@
+package coreservlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class logFilter implements Filter {
+
+	private ServletContext servletContext = null;
+	
+	private FilterConfig filterConfig = null;
+	
+	private String servletName = null;
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest)arg0;
+		servletContext.log("servletName:" + servletName + " " + 
+			"remoteHost:" + request.getRemoteHost() + " " + 
+			"requestURL:" + request.getRequestURL());
+		arg2.doFilter(arg0, arg1);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		this.filterConfig = arg0;
+		this.servletContext = arg0.getServletContext();
+		this.servletName = arg0.getFilterName();
+	}
+
+}
